5bd9d7a81280a06cc08d2bfadefa73a91ededdb6
rogeriojunior92 <rgo_junior@hotmail.com>
2022-06-23 16:34:42 -0300

    Tabuada Python

---
+++ tabuada.py
@@ -0,0 +1,22 @@
+# TABUADA PYTHON, UTILIZANDO LOOP WHILE.
+
+def titulo(txt):
+    print("\033[96m-\033[0;0m" *30)
+    print('\033[96m'+txt+'\033[0;0m')
+    print("\033[96m-\033[0;0m" *30)
+
+titulo("TABUADA PYTHON".center(30))
+while True:
+    valor = int(input("Quer ver qual tabuada? "))
+    for c in range(1, 11):
+        print(f"{valor} x {c} = {valor*c}")
+    
+    while True:
+        resp = input("Quer continuar? [S/N] ").upper()[0]
+        print("\033[96m-\033[0;0m" *30)
+        if resp in "SN":
+            break
+        print("ERRO! Digite apenas S ou N")
+    if resp == "N":
+        break
+print("\033[96m-\033[0;0m" *30)
