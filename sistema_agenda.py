481450a13ffe37e441bcdd9d987fdc1508cc2c40
rogeriojunior92 <rgo_junior@hotmail.com>
2022-06-27 18:44:37 -0300

    Nova função, enviar relatorio excel

--- sistema_agenda.py
+++ sistema_agenda.py
@@ -166,14 +166,14 @@ def enviar_email():
     try:
         outlook = win32.Dispatch('outlook.application')
         email = outlook.CreateItem(0)
-        email.To = "rgo_junior@hotmail.com"
+        email.To = "seu_email@email.com"
         email.Subject = "Cadastro de Agenda"
         email.HTMLBody = """
         <p>Olá,</p>
         
         <p>Segue a lista de agenda cadastrado.</p>  
         """
-        anexo = "C:/Users/Dell/Documents/Eng/Projetos/cadastro_agenda.txt"
+        anexo = "C:/Users/Dell/Documents/Eng/Projetos/lista_agenda.xlsx"
         email.Attachments.Add(anexo)
         email.Save()
         email.Send()
